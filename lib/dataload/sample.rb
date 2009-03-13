12d8bcd11f6050ab96dc2c21c35d06e72ed442fa
pci\mharris <pci\mharris@f6c9f1ed-0ca6-2e48-b550-0345cde077c8>
2009-03-13 18:59:31 +0000

    Made sample.rb better
    git-svn-id: https://svn.princeton.com/svn/pci_share/Dataload@310 f6c9f1ed-0ca6-2e48-b550-0345cde077c8

--- lib/dataload/sample.rb
+++ lib/dataload/sample.rb
@@ -1,6 +1,24 @@
+require 'rubygems'
+require 'dataload'
+
+#setup the sample source file
+source_filename = File.dirname(__FILE__) + "/sample_source.csv"
+source_text = <<EOF
+name,age,city,state
+Bob Smith,24,Atlanta,GA
+Jane Doe,35,Buffalo,NY
+Evan Stein,31,Princeton,NJ
+EOF
+File.create(source_filename,source_text)
+
+#load into a database, creating the table if needed
 dataload do
-  source "source.csv"
+  source source_filename
   database :adapter => 'sqlite3', :database => "db.sqlite3", :timeout => 5000
-  table 'foobar'
-  string(:cat) { bar + "_cat" }
+  table 'people'
+  string(:full_name) { name }
+  string(:first_name) { name.split[0] }
+  string(:last_name) { name.split[1] }
+  integer(:age)
+  string(:city_state) { "#{city}, #{state}" }
 end
