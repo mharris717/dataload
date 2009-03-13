46b4a61a032a8e13a984273abc0aa97e570de982
pci\mharris <pci\mharris@f6c9f1ed-0ca6-2e48-b550-0345cde077c8>
2009-03-13 18:59:33 +0000

    enriched sample
    git-svn-id: https://svn.princeton.com/svn/pci_share/Dataload@312 f6c9f1ed-0ca6-2e48-b550-0345cde077c8

--- lib/dataload/sample.rb
+++ lib/dataload/sample.rb
@@ -13,9 +13,28 @@ File.create(source_filename,source_text)
 
 #load into a database, creating the table if needed
 dataload do
+  # csv file the data is being sourced from
   source source_filename
+  
+  # database/table the data should be loaded into.
+  # the table will be created if it does not already exist
   database :adapter => 'sqlite3', :database => "db.sqlite3", :timeout => 5000
   table 'people'
+  
+  # columns in the new table
+  # available types are string, text, integer, float, decimal, datetime, timestamp, time, date, binary, boolean
+  #
+  # The first argument is the name of the new column
+  # The block describes the value to be populated
+  #
+  # Example: string(:full_name) { name } 
+  # This creates a field 'full_name' of type string in the new table, and populates it with the name field from the csv
+  #
+  # Example: boolean(:is_tall) { height_in_inches.to_i > 74 }
+  # Creates a field 'is_tall' and populates with true if the height_in_inches field in the csv is greater than 74
+  #
+  # A column without a block just passes through the same field in the csv
+  # integer(:age) creates an integer field 'age' in the new table, populated with the age field in the csv
   string(:full_name) { name }
   string(:first_name) { name.split[0] }
   string(:last_name) { name.split[1] }
